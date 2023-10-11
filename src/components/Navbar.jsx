b8ad1fac9c120679585930c93498aef81c2dbd77
Mario <pwarsrawp@gmail.com>
2023-10-11 17:57:17 +0200

    auth improvements, names to list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,66 @@
 import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
+import styled from "styled-components";
 
 function Navbar() {
   const { isLoggedIn } = useContext(AuthContext);
 
   return (
-    <nav>
-      <Link to="/" className='title'>Inicio</Link>
+    <NavContainer>
+      <Link to="/" className="title">
+        Inicio
+      </Link>
       <ul>
-        <li><NavLink to="/activities">Actividades</NavLink></li>
-        {!isLoggedIn && <li><NavLink to="/signup">Registro</NavLink></li>}
-        {!isLoggedIn && <li><NavLink to="/login">Acceso</NavLink></li>}
-        {isLoggedIn && <li><NavLink to="/profile">Mi cuenta</NavLink></li>}
+        <li>
+          <NavLink to="/activities">Actividades</NavLink>
+        </li>
+        {!isLoggedIn && (
+          <li>
+            <NavLink to="/signup">Registro</NavLink>
+          </li>
+        )}
+        {!isLoggedIn && (
+          <li>
+            <NavLink to="/login">Acceso</NavLink>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <NavLink to="/profile">Mi cuenta</NavLink>
+          </li>
+        )}
       </ul>
-    </nav>
+    </NavContainer>
   );
 }
 
 export default Navbar;
+
+const NavContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #0ead99;
+  .title {
+    font-size: 1.5rem;
+    margin: 1rem;
+    font-weight: 600;
+    text-decoration: none;
+    color: white;
+  }
+  ul {
+    display: flex;
+  }
+  ul li {
+    list-style: none;
+  }
+  ul li a {
+    display: block;
+    text-decoration: none;
+    color: white;
+    padding: 0.5rem;
+    margin: 0 0.5rem;
+    border-radius: 0.3rem;
+  }
+`;
--- src/pages/ActivityDetails.jsx
+++ src/pages/ActivityDetails.jsx
@@ -15,7 +15,9 @@ function ActivityDetails() {
   useEffect(() => {
     const getOneActivity = async () => {
       try {
-        const oneActivity = await axios.get(`${api_url}/activities/${activityId}`);
+        const oneActivity = await axios.get(
+          `${api_url}/activities/${activityId}`
+        );
         // const waitingList = await axios.get(`${api_url}/lists/${activityId}`);
         setActivity(oneActivity.data);
         // setWaitingList(waitingList.data);
@@ -32,24 +34,53 @@ function ActivityDetails() {
     <div className="activity-details-wrapper">
       <h1>{activity.name}</h1>
       <br />
-      <h3>Ciclo: <span>{activity.cicle}</span></h3>
-      <h3>Nivel: <span>{activity.level}</span></h3>
-      <h3>Nº días: <span>{activity.numberDays}</span></h3>
-      <h3>Días: <span>{activity.daysWeek}</span></h3>
-      <h3>Horario: <span>{activity.schedule}</span></h3>
-      <h3>Precio socios: <span>{activity.memberPrice} €/mes</span></h3>
-      <h3>Precio no socios: <span>{activity.noMemberPrice} €/mes</span></h3>
-      <h3>Empresa: <span>{activity.company}</span></h3>
-      <h3>Nº plazas: <span>{activity.slots}</span></h3>
+      <h3>
+        Ciclo: <span>{activity.cicle}</span>
+      </h3>
+      <h3>
+        Nivel: <span>{activity.level}</span>
+      </h3>
+      <h3>
+        Nº días: <span>{activity.numberDays}</span>
+      </h3>
+      <h3>
+        Días: <span>{activity.daysWeek}</span>
+      </h3>
+      <h3>
+        Horario: <span>{activity.schedule}</span>
+      </h3>
+      <h3>
+        Precio socios: <span>{activity.memberPrice} €/mes</span>
+      </h3>
+      <h3>
+        Precio no socios: <span>{activity.noMemberPrice} €/mes</span>
+      </h3>
+      <h3>
+        Empresa: <span>{activity.company}</span>
+      </h3>
+      <h3>
+        Nº plazas: <span>{activity.slots}</span>
+      </h3>
       {user?.isAdmin && (
         <div className="activity-details-buttons">
           <button>Editar</button>
           <button>Borrar</button>
         </div>
       )}
+      <br />
+      <br />
       <div className="activity-details-waiting-list">
         <h3>Lista de espera</h3>
-        {activity.list.map((student, index) => <p key={index}>{index + 1} {student}</p>)}
+        <br />
+        {activity.list.map((student, index) => (
+          <div key={index} style={{display: 'flex', gap: '20px', marginBottom: '15px'}}>
+            <h2>{index + 1}</h2>
+            <p>
+               {student}
+            </p>
+            <br />
+          </div>
+        ))}
       </div>
     </div>
   );
